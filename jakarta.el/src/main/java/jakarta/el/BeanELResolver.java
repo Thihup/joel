c683163d07ebe7bd5bd781b7073f4c4996cb7cd5
Thiago Henrique Hupner <thihup@gmail.com>
2021-04-20 11:36:17 -0300

    Initial implementation of BeanELResolver

--- jakarta.el/src/main/java/jakarta/el/BeanELResolver.java
+++ jakarta.el/src/main/java/jakarta/el/BeanELResolver.java
@@ -1,6 +1,10 @@
 package jakarta.el;
 
 import java.beans.FeatureDescriptor;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.reflect.Method;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.Objects;
 
@@ -62,29 +66,23 @@ public class BeanELResolver extends ELResolver {
     }
 
     /**
-     * If the base object is not <code>null</code>, returns an <code>Iterator</code> containing the set of JavaBeans
-     * properties available on the given object. Otherwise, returns <code>null</code>.
+     * If the base object is not <code>null</code>, returns the most general type that this resolver accepts for the
+     * <code>property</code> argument. Otherwise, returns <code>null</code>.
      *
      * <p>
-     * The <code>Iterator</code> returned must contain zero or more instances of {@link java.beans.FeatureDescriptor}. Each
-     * info object contains information about a property in the bean, as obtained by calling the
-     * <code>BeanInfo.getPropertyDescriptors</code> method. The <code>FeatureDescriptor</code> is initialized using the same
-     * fields as are present in the <code>PropertyDescriptor</code>, with the additional required named attributes
-     * "<code>type</code>" and "<code>resolvableAtDesignTime</code>" set as follows:
-     * <ul>
-     * <li>{@link ELResolver#TYPE} - The runtime type of the property, from
-     * <code>PropertyDescriptor.getPropertyType()</code>.</li>
-     * <li>{@link ELResolver#RESOLVABLE_AT_DESIGN_TIME} - <code>true</code>.</li>
-     * </ul>
+     * Assuming the base is not <code>null</code>, this method will always return <code>Object.class</code>. This is because
+     * any object is accepted as a key and is coerced into a string.
+     * </p>
      *
      * @param context The context of this evaluation.
-     * @param base    The bean to analyze.
-     * @return An <code>Iterator</code> containing zero or more <code>FeatureDescriptor</code> objects, each representing a
-     * property on this bean, or <code>null</code> if the <code>base</code> object is <code>null</code>.
+     * @param base The bean to analyze.
+     * @return <code>null</code> if base is <code>null</code>; otherwise <code>Object.class</code>.
      */
     @Override
     public Class<?> getCommonPropertyType(ELContext context, Object base) {
-        return null;
+        if (base == null)
+            return null;
+        return Object.class;
     }
 
     /**
@@ -110,7 +108,9 @@ public class BeanELResolver extends ELResolver {
      */
     @Override
     public Iterator<FeatureDescriptor> getFeatureDescriptors(ELContext context, Object base) {
-        return null;
+        if (base == null)
+            return null;
+        return Collections.emptyIterator();
     }
 
     /**
@@ -143,9 +143,22 @@ public class BeanELResolver extends ELResolver {
     @Override
     public Class<?> getType(ELContext context, Object base, Object property) {
         Objects.requireNonNull(context);
-        if (base == null)
+        if (base == null || property == null)
             return null;
-        return null;
+        Class<?> aClass = base.getClass();
+        Method method;
+        String methodName = property.toString();
+        try {
+            method = aClass.getMethod("get" + methodName.substring(0, 1).toUpperCase() + methodName.substring(1));
+        } catch (NoSuchMethodException e) {
+            try {
+                method = aClass.getMethod("is" + methodName.substring(0, 1).toUpperCase() + methodName.substring(1));
+            } catch (NoSuchMethodException root) {
+                throw new PropertyNotFoundException(String.format("Property %s not found", property));
+            }
+        }
+        context.setPropertyResolved(base, property);
+        return method.getReturnType();
     }
 
     /**
@@ -178,7 +191,26 @@ public class BeanELResolver extends ELResolver {
     @Override
     public Object getValue(ELContext context, Object base, Object property) {
         Objects.requireNonNull(context);
-        return null;
+        if (base == null || property == null)
+            return null;
+        Class<?> aClass = base.getClass();
+        Method method;
+        String methodName = property.toString();
+        try {
+            method = aClass.getMethod("get" + methodName.substring(0, 1).toUpperCase() + methodName.substring(1));
+        } catch (NoSuchMethodException e) {
+            try {
+                method = aClass.getMethod("is" + methodName.substring(0, 1).toUpperCase() + methodName.substring(1));
+            } catch (NoSuchMethodException root) {
+                throw new PropertyNotFoundException(String.format("Property %s not found", property));
+            }
+        }
+        try {
+            context.setPropertyResolved(base, property);
+            return MethodHandles.lookup().unreflect(method).bindTo(base).invoke();
+        } catch (Throwable e) {
+            throw new ELException(e);
+        }
     }
 
     /**
@@ -215,7 +247,90 @@ public class BeanELResolver extends ELResolver {
     @Override
     public boolean isReadOnly(ELContext context, Object base, Object property) {
         Objects.requireNonNull(context);
-        return readyOnly;
+        Objects.requireNonNull(context);
+        if (base == null || property == null)
+            return false;
+        if (readyOnly)
+            throw new PropertyNotWritableException();
+        Class<?> aClass = base.getClass();
+        String propertyName = property.toString();
+        Method method;
+        try {
+            method = aClass.getMethod("get" + propertyName.substring(0, 1).toUpperCase() + propertyName.substring(1));
+        } catch (NoSuchMethodException e) {
+            try {
+                method = aClass.getMethod("is" + propertyName.substring(0, 1).toUpperCase() + propertyName.substring(1));
+            } catch (NoSuchMethodException root) {
+                throw new PropertyNotFoundException(String.format("Property %s not found", property));
+            }
+        }
+        try {
+            aClass.getMethod("set" + propertyName.substring(0, 1).toUpperCase() + propertyName.substring(1), method.getReturnType());
+            context.setPropertyResolved(base, property);
+            return false;
+        } catch (Throwable e) {
+            throw new PropertyNotWritableException(e);
+        }
+    }
+
+    /**
+     * If the base object is not <code>null</code>, invoke the method, with the given parameters on this bean. The return
+     * value from the method is returned.
+     *
+     * <p>
+     * If the base is not <code>null</code>, the <code>propertyResolved</code> property of the <code>ELContext</code> object
+     * must be set to <code>true</code> by this resolver, before returning. If this property is not <code>true</code> after
+     * this method is called, the caller should ignore the return value.
+     * </p>
+     *
+     * <p>
+     * The provided method object will first be coerced to a <code>String</code>. The methods in the bean is then examined
+     * and an attempt will be made to select one for invocation. If no suitable can be found, a
+     * <code>MethodNotFoundException</code> is thrown.
+     * <p>
+     * If the given paramTypes is not <code>null</code>, select the method with the given name and parameter types.
+     * <p>
+     * Else select the method with the given name that has the same number of parameters. If there are more than one such
+     * method, the method selection process is undefined.
+     * <p>
+     * Else select the method with the given name that takes a variable number of arguments.
+     * <p>
+     * Note the resolution for overloaded methods will likely be clarified in a future version of the spec.
+     * <p>
+     * The provide parameters are coerced to the corresponding parameter types of the method, and the method is then
+     * invoked.
+     *
+     * @param context        The context of this evaluation.
+     * @param base           The bean on which to invoke the method
+     * @param method         The simple name of the method to invoke. Will be coerced to a <code>String</code>. If method is
+     *                       "&lt;init&gt;"or "&lt;clinit&gt;" a MethodNotFoundException is thrown.
+     * @param parameterTypes An array of Class objects identifying the method's formal parameter types, in declared order. Use
+     *                       an empty array if the method has no parameters. Can be <code>null</code>, in which case the method's formal parameter
+     *                       types are assumed to be unknown.
+     * @param params         The parameters to pass to the method, or <code>null</code> if no parameters.
+     * @return The result of the method invocation (<code>null</code> if the method has a <code>void</code> return type).
+     * @throws MethodNotFoundException if no suitable method can be found.
+     * @throws ELException             if an exception was thrown while performing (base, method) resolution. The thrown exception must
+     *                                 be included as the cause property of this exception, if available. If the exception thrown is an
+     *                                 <code>InvocationTargetException</code>, extract its <code>cause</code> and pass it to the <code>ELException</code>
+     *                                 constructor.
+     * @since Jakarta Expression Language 2.2
+     */
+    @Override
+    public Object invoke(ELContext context, Object base, Object method, Class<?>[] parameterTypes, Object[] params) {
+        Objects.requireNonNull(context);
+        if (base == null || method == null)
+            return null;
+        Class<?> aClass = base.getClass();
+        try {
+            MethodHandle unreflect = MethodHandles.lookup().unreflect(aClass.getMethod(method.toString(), parameterTypes));
+            context.setPropertyResolved(base, method);
+            return unreflect.bindTo(base).invokeWithArguments(params);
+        } catch (NoSuchMethodException e) {
+            throw new MethodNotFoundException(e);
+        } catch (Throwable exception) {
+            throw new ELException(exception);
+        }
     }
 
     /**
@@ -254,5 +369,27 @@ public class BeanELResolver extends ELResolver {
     @Override
     public void setValue(ELContext context, Object base, Object property, Object value) {
         Objects.requireNonNull(context);
+        if (base == null || property == null)
+            return;
+        if (readyOnly)
+            throw new PropertyNotWritableException();
+        Class<?> aClass = base.getClass();
+        String propertyName = property.toString();
+        try {
+            aClass.getMethod("get" + propertyName.substring(0, 1).toUpperCase() + propertyName.substring(1));
+        } catch (NoSuchMethodException e) {
+            try {
+                aClass.getMethod("is" + propertyName.substring(0, 1).toUpperCase() + propertyName.substring(1));
+            } catch (NoSuchMethodException root) {
+                throw new PropertyNotFoundException(String.format("Property %s not found", property));
+            }
+        }
+        try {
+            MethodHandle unreflect = MethodHandles.lookup().unreflect(aClass.getMethod("set" + propertyName.substring(0, 1).toUpperCase() + propertyName.substring(1), value.getClass()));
+            context.setPropertyResolved(base, property);
+            unreflect.bindTo(base).invoke(value);
+        } catch (Throwable e) {
+            throw new ELException(e);
+        }
     }
 }
