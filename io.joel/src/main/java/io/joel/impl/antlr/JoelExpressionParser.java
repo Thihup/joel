6a874ee62c17840f596c5e369b1f529af919f4bd
Thiago Henrique Hupner <thihup@gmail.com>
2021-04-25 13:27:06 -0300

    Cache expression parsing

--- io.joel/src/main/java/io/joel/impl/antlr/JoelExpressionParser.java
+++ io.joel/src/main/java/io/joel/impl/antlr/JoelExpressionParser.java
@@ -8,21 +8,39 @@ import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.RecognitionException;
 import org.antlr.v4.runtime.Recognizer;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class JoelExpressionParser {
+    private static final Map<String, ExpressionNode> CACHED_EXPRESSION = new HashMap<>();
+    private static final ELErrorListener ERROR_LISTENER = new ELErrorListener();
+
     private JoelExpressionParser() {
     }
 
     public static ExpressionNode parse(String expression) {
-        var lexer = new ExpressionLanguageGrammarLexer(CharStreams.fromString(expression));
-        var parser = new ExpressionLanguageGrammarParser(new CommonTokenStream(lexer));
-        parser.removeErrorListeners();
-        parser.addErrorListener(new BaseErrorListener() {
-            @Override
-            public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
-                throw new ELException(String.format("line %d:%d %s", line, charPositionInLine, msg));
+        return CACHED_EXPRESSION.computeIfAbsent(expression, key -> {
+            var lexer = new ExpressionLanguageGrammarLexer(CharStreams.fromString(expression));
+            var parser = new ExpressionLanguageGrammarParser(new CommonTokenStream(lexer));
+            parser.removeErrorListeners();
+            parser.addErrorListener(ERROR_LISTENER);
+            var prog = parser.prog();
+            ExpressionNode visit = new ExpressionVisitor().visit(prog);
+            if (visit instanceof ExpressionNode.DynamicExpressionNode dynamicExpression) {
+                return dynamicExpression.node();
             }
+            if (visit instanceof ExpressionNode.DeferredExpressionNode deferredExpression) {
+                return deferredExpression.node();
+            }
+            return visit;
         });
-        var prog = parser.prog();
-        return new ExpressionVisitor().visit(prog);
     }
+
+    private static class ELErrorListener extends BaseErrorListener {
+        @Override
+        public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
+            throw new ELException(String.format("line %d:%d %s", line, charPositionInLine, msg));
+        }
+    }
+
 }
